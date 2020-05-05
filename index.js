aeee88eea15e1c2aa3d2927cddd3925532423a4d
Vinicius Holanda <holanda796@gmail.com>
2020-05-04 21:05:22 -0300

    alteração de codigo para for

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert ("hello world")
+
+var numberOfDrumBottons = document.querySelectorAll(".drum").length
+
+for (let i = 0; i < numberOfDrumBottons; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        alert("I got clicked");
+        
+    
+    })
+}
+
+    
+
